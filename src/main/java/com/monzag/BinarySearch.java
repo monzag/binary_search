c1748016d52432301ce49dd057afc51d92de0aa2
monzag <zaganiacz.m@gmail.com>
2018-02-06 17:12:06 +0100

    Add search method

--- src/main/java/com/monzag/BinarySearch.java
+++ src/main/java/com/monzag/BinarySearch.java
@@ -1,7 +1,28 @@
 package com.monzag;
 
+public class BinarySearch {
 
-public class BinarySearch<T> {
+    public Boolean search(int[] numbers, int toFind) {
+        numbers = sort(numbers);
+        int startPoint = 0;
+        int endPoint = numbers.length - 1;
+        Boolean notFound = true;
+        while (notFound) {
+            int middlePoint = (startPoint + endPoint) / 2;
 
+            if (startPoint > endPoint) {
+                notFound = false;
 
+            } if (numbers[middlePoint] == toFind) {
+                return true;
+
+            } if (numbers[middlePoint] > toFind) {
+                endPoint = middlePoint - 1;
+
+            } else {
+                startPoint = middlePoint + 1;
+            }
+        }
+        return notFound;
+    }
 }
