32829c378354c9461be7aa666e1d1b6777d637a1
monzag <zaganiacz.m@gmail.com>
2018-02-06 17:21:00 +0100

    Add BinarySearchTest and setup method

---
+++ src/test/java/com/monzag/BinarySearchTest.java
@@ -0,0 +1,16 @@
+package com.monzag;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinarySearchTest {
+
+    @BeforeEach
+    void setup() {
+        BinarySearch searcher = new BinarySearch();
+    }
+
+
+}
